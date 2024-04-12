c7af9109334c38ceee899fe8ca3221da8845a55b
Bhumika Dixit <bhumikadixit990@gmail.com>
2024-04-12 22:11:03 +0530

    first commit

---
+++ cache_utils.py
@@ -0,0 +1,27 @@
+import frappe
+
+@frappe.whitelist()
+def get_cached_data():
+    cached_data = frappe.cache().get_value("cached_data")
+
+    if cached_data:
+        return cached_data
+    else:
+        data = fetch_data_from_database()
+
+        frappe.cache().set_value("cached_data", data)
+
+        return data
+
+def fetch_data_from_database():
+    items = frappe.get_all("Item", fields=["item_code", "item_group"])
+
+    formatted_data = []
+    for item in items:
+        formatted_data.append({
+            "item_code": item.item_code,
+            "item_group": item.item_group,
+        })
+
+    return formatted_data
+
